6fe8e018c38598b7de4247a5a40272ee0eb23c6f
dev8 <yuka@MacBook-Air.local>
2024-07-28 23:50:28 +0900

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
