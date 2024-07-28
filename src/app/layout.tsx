e64ff2eeffcc674ffad9b4b88049f0deee70cb56
dev8 <yuka@MacBook-Air.local>
2024-07-29 00:28:48 +0900

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
