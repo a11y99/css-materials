8749915ce685a53fa79742d89a7f205d99dfad23
dev8 <yuka@MacBook-Air.local>
2024-08-01 10:33:22 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,49 +13,47 @@ export default function Home() {
   return (
     <div className="w-[80%] mx-auto">
       <Header />
-      <div className="mt-10 flex flex-col items-center">
-        <img src="/cheese.png" className="w-[100px]" />
-        <img src="/text-logo.png" className="w-[200px]" />
-      </div>
-      <div className="mt-10 space-x-2.5 flex items-center justify-center">
-        <Link href="/docs/getting-started"><Button colorScheme='blue'>Get Started</Button></Link>
-        <Link href="https://github.com/hn-104/css-materials"><Button colorScheme='gray'><FaGithub className="mr-2.5 text-lg" />GitHub</Button></Link>
-      </div>
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-5">
-        <Card title="CSS Materials Launches Beta Version!">
-          <div className="flex flex-col h-20">
-            <p className="opacity-50 text-sm">Amazing Collection of Easy-to-Use Components!</p>
-            <p className="mt-auto text-sm">2024-07-31</p>
-          </div>
-        </Card>
-        <Card title="XXXXX XXXXX XXXXX XXXXX">
-          <div className="flex flex-col h-20">
-            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
-            <p className="mt-auto text-sm">20XX-XX-XX</p>
-          </div>
-        </Card>
-        <Card title="XXXXX XXXXX XXXXX XXXXX">
-          <div className="flex flex-col h-20">
-            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
-            <p className="mt-auto text-sm">20XX-XX-XX</p>
-          </div>
-        </Card>
-      </div>
       <div className="mt-10 grid grid-cols-1 md:grid-cols-2">
-        <div>
+        <div className="hidden md:block">
           <img src="/chubbs/2.svg" />
         </div>
-        <div className="space-y-10">
-          <div className="space-y-5">
-            <h1 className="text-2xl font-bold">Abundant Components</h1>
-            <p className="text-lg opacity-50">A Collection of Beginner-Friendly, Easy-to-Use Components!</p>
+        <div className="space-y-5">
+          <div className="flex flex-col items-center">
+            <img src="/cheese.png" className="w-[100px]" />
+            <img src="/text-logo.png" className="w-[200px]" />
           </div>
-          <div>
-            <Link href="/components">
-              <Button colorScheme='purple'>Browse Components</Button>
-            </Link>
+          <div className="space-x-2.5 flex items-center justify-center">
+            <Link href="/docs/getting-started"><Button colorScheme='blue'>Get Started</Button></Link>
+            <Link href="https://github.com/hn-104/css-materials"><Button colorScheme='gray'><FaGithub className="mr-2.5 text-lg" />GitHub</Button></Link>
           </div>
+          <p className="opacity-50">A Collection of Beginner-Friendly, Easy-to-Use Components!</p>
+          <Card title="CSS Materials Launches Beta Version!">
+            <div className="flex flex-col h-20">
+              <p className="opacity-50 text-sm">Amazing Collection of Easy-to-Use Components!</p>
+              <p className="mt-auto text-sm">2024-07-31</p>
+            </div>
+          </Card>
         </div>
+      </div>
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-5">
+        <Card title="XXXXX XXXXX XXXXX XXXXX">
+          <div className="flex flex-col h-20">
+            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
+            <p className="mt-auto text-sm">20XX-XX-XX</p>
+          </div>
+        </Card>
+        <Card title="XXXXX XXXXX XXXXX XXXXX">
+          <div className="flex flex-col h-20">
+            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
+            <p className="mt-auto text-sm">20XX-XX-XX</p>
+          </div>
+        </Card>
+        <Card title="XXXXX XXXXX XXXXX XXXXX">
+          <div className="flex flex-col h-20">
+            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
+            <p className="mt-auto text-sm">20XX-XX-XX</p>
+          </div>
+        </Card>
       </div>
     </div>
   );
