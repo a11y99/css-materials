7c57f2c76e5a72cc45d4ad10bb3ce18a923fdf98
hono <m.hono.104.110@gmail.com>
2024-08-01 10:57:27 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="hidden md:block">
           <img src="/chubbs/2.svg" />
         </div>
-        <div className="space-y-5">
+        <div className="space-y-5 flex flex-col">
           <div className="flex flex-col items-center">
             <img src="/cheese.png" className="w-[100px]" />
             <img src="/text-logo.png" className="w-[200px]" />
@@ -27,19 +27,13 @@ export default function Home() {
             <Link href="https://github.com/hn-104/css-materials"><Button colorScheme='gray'><FaGithub className="mr-2.5 text-lg" />GitHub</Button></Link>
           </div>
           <p className="opacity-50">A Collection of Beginner-Friendly, Easy-to-Use Components!</p>
-          <Card title="CSS Materials Launches Beta Version!">
-            <div className="flex flex-col h-20">
-              <p className="opacity-50 text-sm">Amazing Collection of Easy-to-Use Components!</p>
-              <p className="mt-auto text-sm">2024-07-31</p>
-            </div>
-          </Card>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-        <Card title="XXXXX XXXXX XXXXX XXXXX">
+      <div className="mt-10 md:mt-0 grid grid-cols-1 md:grid-cols-3 gap-5">
+        <Card title="CSS Materials Launches Beta Version!">
           <div className="flex flex-col h-20">
-            <p className="opacity-50 text-sm">XXXXX XXXXX XXXXX XXXXX XXXXX XXXXX</p>
-            <p className="mt-auto text-sm">20XX-XX-XX</p>
+            <p className="opacity-50 text-sm">Amazing Collection of Easy-to-Use Components!</p>
+            <p className="mt-auto text-sm">2024-07-31</p>
           </div>
         </Card>
         <Card title="XXXXX XXXXX XXXXX XXXXX">
