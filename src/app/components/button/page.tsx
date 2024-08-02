2dd699695ffa9af72a89b41cc02e5fa414370c0c
hono <m.hono.104.110@gmail.com>
2024-08-02 18:52:59 +0900

    Update Button Component

--- src/app/components/button/page.tsx
+++ src/app/components/button/page.tsx
@@ -7,16 +7,48 @@ import Code from '@/components/Code';
 import Body from '@/components/Body';
 import PreviewAreaH from '@/components/PreviewAreaH';
 
+const texts = {
+    en: {
+        overview: "Overview",
+        overviewDesc: "Button is a clickable element that allows users to perform actions component.",
+        import: "Import",
+        usage: "Usage",
+        changeSize: "Change Size",
+        changeSizeDesc: "By default, the medium size is applied.",
+        changeShape: "Change Shape",
+        changeShapeDesc: "By default, the rounded shape is applied.",
+        changeColorscheme: "Change Colorscheme",
+        changeColorschemeDesc: "By default, the blue color is applied.",
+        changeState: "Change State",
+        changeStateDesc: "To change the state, set either disabled or loading to true.",
+    },
+    jp: {
+        overview: "概要",
+        overviewDesc: "Buttonは、ユーザーがアクションを実行するためのクリック可能な要素コンポーネントです。",
+        import: "インポート",
+        usage: "使い方",
+        changeSize: "サイズを変更する",
+        changeSizeDesc: "デフォルトではmediumが適用されています。",
+        changeShape: "形状を変更する",
+        changeShapeDesc: "デフォルトではroundedが適用されています。",
+        changeColorscheme: "カラースキームを変更する",
+        changeColorschemeDesc: "デフォルトではblueが適用されています。",
+        changeState: "状態を変更する",
+        changeStateDesc: "状態を変更するにはdisabledとloadingのそれぞれをtrueにします。",
+    }
+};
+
 const ButtonComponents = () => {
     const [language, setLanguage] = useState<'en' | 'jp'>('en');
+
+    const t = texts[language];
 
     return (
         <Body>
             <Header />
-            <div className="mb-10 text-center flex flex-col items-center">
-                <img src="/chubbs/6.svg" className="w-80" />
-                <h1 className="text-xl font-bold mt-[-50px]">Button</h1>
-                <div className="mt-5">
+            <div className="mt-20 mb-10 flex flex-col space-y-2.5">
+                <h1 className="text-[32px] font-bold">Button</h1>
+                <div>
                     <button
                         className={`px-4 py-2 font-bold rounded-l-md ${language === 'en' ? 'bg-[#2da0ff] text-white' : 'bg-gray-100'}`}
                         onClick={() => setLanguage('en')}
@@ -24,7 +56,7 @@ const ButtonComponents = () => {
                         English
                     </button>
                     <button
-                        className={`px-4 py-2 font-bold rounded-r-md ${language === 'jp' ? 'bg-[#2da0ff] text-white' : 'bg-gray-100'}`}
+                    className={`px-4 py-2 font-bold rounded-r-md ${language === 'jp' ? 'bg-[#2da0ff] text-white' : 'bg-gray-100'}`}
                         onClick={() => setLanguage('jp')}
                     >
                         日本語
@@ -32,153 +64,76 @@ const ButtonComponents = () => {
                 </div>
             </div>
             <div className="space-y-5">
-                {language === 'en' ? (
-                    <>
-                        <Card title="Overview">
-                            Button is a clickable element that allows users to perform actions component.
-                        </Card>
-                        <Card title="Import">
-                            <Code language='tsx'>{`import { Button } from "css-materials";`}</Code>
-                        </Card>
-                        <Card title="Usage">
-                            <PreviewAreaH>
-                                <Button>Button</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>{`<Button>Button</Button>`}</Code>
-                        </Card>
-                        <Card title="Change Size">
-                            <p>By default, the <code>medium</code> size is applied.</p>
-                            <PreviewAreaH>
-                                <Button size="small">Small</Button>
-                                <Button size="medium">Medium</Button>
-                                <Button size="large">Large</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
+                <Card title={t.overview}>
+                    {t.overviewDesc}
+                </Card>
+                <Card title={t.import}>
+                    <Code language='tsx'>{`import { Button } from "css-materials";`}</Code>
+                </Card>
+                <Card title={t.usage}>
+                    <PreviewAreaH>
+                        <Button>Button</Button>
+                    </PreviewAreaH>
+                    <Code language='html'>{`<Button>Button</Button>`}</Code>
+                </Card>
+                <Card title={t.changeSize}>
+                    <p>{t.changeSizeDesc}</p>
+                    <PreviewAreaH>
+                        <Button size="small">Small</Button>
+                        <Button size="medium">Medium</Button>
+                        <Button size="large">Large</Button>
+                    </PreviewAreaH>
+                    <Code language='html'>
 {`<Button size="small">Small</Button>
 <Button size="medium">Medium</Button>
 <Button size="large">Large</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="形状を変更する">
-                            <p>デフォルトでは<code>rounded</code>が適用されています。</p>
-                            <PreviewAreaH>
-                                <Button shape="square">四角ボタン</Button>
-                                <Button shape="rounded">角丸ボタン</Button>
-                                <Button shape="circular">円形ボタン</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
-{`<Button shape="square">四角ボタン</Button>
-<Button shape="rounded">角丸ボタン</Button>
-<Button shape="circular">円形ボタン</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="カラースキームを変更する">
-                            <p>デフォルトでは<code>blue</code>が適用されています。</p>
-                            <PreviewAreaH>
-                                <Button colorScheme="red">まめだまし</Button>
-                                <Button colorScheme="orange">まめだまし</Button>
-                                <Button colorScheme="yellow">まめだまし</Button>
-                                <Button colorScheme="green">まめだまし</Button>
-                                <Button colorScheme="blue">まめだまし</Button>
-                                <Button colorScheme="purple">まめだまし</Button>
-                                <Button colorScheme="gray">まめだまし</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
-{`<Button colorScheme="red">まめだまし</Button>
-<Button colorScheme="orange">まめだまし</Button>
-<Button colorScheme="yellow">まめだまし</Button>
-<Button colorScheme="green">まめだまし</Button>
-<Button colorScheme="blue">まめだまし</Button>
-<Button colorScheme="purple">まめだまし</Button>
-<Button colorScheme="gray">まめだまし</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="状態を変更する">
-                            <p>状態を変更するには<code>disabled</code>と<code>loading</code>のそれぞれを<code>true</code>にします。</p>
-                            <div className="flex border p-2.5 rounded-md space-x-2.5 overflow-x-auto whitespace-nowrap">
-                                <Button disabled>無効</Button>
-                                <Button loading>読み込み</Button>
-                            </div>
-                            <Code language='html'>
-{`<Button disabled>無効</Button>
-<Button loading>読み込み</Button>`}
-                            </Code>
-                        </Card>
-                    </>
-                ) : (
-                    <>
-                        <Card title="概要">
-                            Buttonは、ユーザーがアクションを実行するためのクリック可能な要素コンポーネントです。
-                        </Card>
-                        <Card title="インポート">
-                            <Code language='tsx'>{`import { Button } from "css-materials";`}</Code>
-                        </Card>
-                        <Card title="使い方">
-                            <PreviewAreaH>
-                                <Button>ねこむすび</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>{`<Button>ねこむすび</Button>`}</Code>
-                        </Card>
-                        <Card title="サイズを変更する">
-                            <p>デフォルトでは<code>medium</code>が適用されています。</p>
-                            <PreviewAreaH>
-                                <Button size="small">いぬだわら</Button>
-                                <Button size="medium">いぬだわら</Button>
-                                <Button size="large">いぬだわら</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
-{`<Button size="small">いぬだわら</Button>
-<Button size="medium">いぬだわら</Button>
-<Button size="large">いぬだわら</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="形状を変更する">
-                            <p>デフォルトでは<code>rounded</code>が適用されています。</p>
-                            <PreviewAreaH>
-                                <Button shape="square">四角ボタン</Button>
-                                <Button shape="rounded">角丸ボタン</Button>
-                                <Button shape="circular">円形ボタン</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
-{`<Button shape="square">四角ボタン</Button>
-<Button shape="rounded">角丸ボタン</Button>
-<Button shape="circular">円形ボタン</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="カラースキームを変更する">
-                            <p>デフォルトでは<code>blue</code>が適用されています。</p>
-                            <PreviewAreaH>
-                                <Button colorScheme="red">まめだまし</Button>
-                                <Button colorScheme="orange">まめだまし</Button>
-                                <Button colorScheme="yellow">まめだまし</Button>
-                                <Button colorScheme="green">まめだまし</Button>
-                                <Button colorScheme="blue">まめだまし</Button>
-                                <Button colorScheme="purple">まめだまし</Button>
-                                <Button colorScheme="gray">まめだまし</Button>
-                            </PreviewAreaH>
-                            <Code language='html'>
-{`<Button colorScheme="red">まめだまし</Button>
-<Button colorScheme="orange">まめだまし</Button>
-<Button colorScheme="yellow">まめだまし</Button>
-<Button colorScheme="green">まめだまし</Button>
-<Button colorScheme="blue">まめだまし</Button>
-<Button colorScheme="purple">まめだまし</Button>
-<Button colorScheme="gray">まめだまし</Button>`}
-                            </Code>
-                        </Card>
-                        <Card title="状態を変更する">
-                            <p>状態を変更するには<code>disabled</code>と<code>loading</code>のそれぞれを<code>true</code>にします。</p>
-                            <div className="flex border p-2.5 rounded-md space-x-2.5 overflow-x-auto whitespace-nowrap">
-                                <Button disabled>無効</Button>
-                                <Button loading>読み込み</Button>
-                            </div>
-                            <Code language='html'>
-{`<Button disabled>無効</Button>
-<Button loading>読み込み</Button>`}
-                            </Code>
-                        </Card>
-                    </>
-                )}
+                    </Code>
+                </Card>
+                <Card title={t.changeShape}>
+                    <p>{t.changeShapeDesc}</p>
+                    <PreviewAreaH>
+                        <Button shape="square">Square</Button>
+                        <Button shape="rounded">Rounded</Button>
+                        <Button shape="circular">Circular</Button>
+                    </PreviewAreaH>
+                    <Code language='html'>
+{`<Button shape="square">Square</Button>
+<Button shape="rounded">Rounded</Button>
+<Button shape="circular">Circular</Button>`}
+                    </Code>
+                </Card>
+                <Card title={t.changeColorscheme}>
+                    <p>{t.changeColorschemeDesc}</p>
+                    <PreviewAreaH>
+                        <Button colorScheme="red">Red</Button>
+                        <Button colorScheme="orange">Orange</Button>
+                        <Button colorScheme="yellow">Yellow</Button>
+                        <Button colorScheme="green">Green</Button>
+                        <Button colorScheme="blue">Blue</Button>
+                        <Button colorScheme="purple">Purple</Button>
+                        <Button colorScheme="gray">Gray</Button>
+                    </PreviewAreaH>
+                    <Code language='html'>
+{`<Button colorScheme="red">Red</Button>
+<Button colorScheme="orange">Orange</Button>
+<Button colorScheme="yellow">Yellow</Button>
+<Button colorScheme="green">Green</Button>
+<Button colorScheme="blue">Blue</Button>
+<Button colorScheme="purple">Purple</Button>
+<Button colorScheme="gray">Gray</Button>`}
+                    </Code>
+                </Card>
+                <Card title={t.changeState}>
+                    <p>{t.changeStateDesc}</p>
+                    <div className="flex border p-2.5 rounded-md space-x-2.5 overflow-x-auto whitespace-nowrap">
+                        <Button colorScheme='gray' disabled>Disabled</Button>
+                        <Button loading>Loading</Button>
+                    </div>
+                    <Code language='html'>
+{`<Button colorScheme='gray' disabled>Disabled</Button>
+<Button loading>Loading</Button>`}
+                    </Code>
+                </Card>
             </div>
         </Body>
     );
