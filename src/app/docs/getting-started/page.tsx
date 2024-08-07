ac5610152f5e83f04bd24dfdb57e843872e231f0
hono <m.hono.104.110@gmail.com>
2024-08-08 00:15:17 +0900

    Update

--- src/app/docs/getting-started/page.tsx
+++ src/app/docs/getting-started/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import Button from '@/components/Button';
+import Button from '@/components/css-materials/Button';
 import Header from '@/components/Header';
 import Card from '@/components/Card';
 import Body from '@/components/Body';
