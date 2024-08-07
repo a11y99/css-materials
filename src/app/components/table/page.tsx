a347645c4dbf9e52a7b103ff2598b4e4ba07eff2
hono <m.hono.104.110@gmail.com>
2024-08-08 00:12:10 +0900

    debug

--- src/app/components/table/page.tsx
+++ src/app/components/table/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useState } from 'react';
-import Button from '@/components/Button';
 import Header from '@/components/Header';
 import Card from '@/components/Card';
 import Body from '@/components/Body';
